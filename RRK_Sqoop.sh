e1f5598cbb189edb63c1879850f4e693ff7c1c08
RVedav <130096240+RVedav@users.noreply.github.com>
2023-06-14 13:37:38 -0500

    changed file format

--- RRK_Sqoop.sh
+++ RRK_Sqoop.sh
@@ -3,7 +3,7 @@ sqoop export \
 --username 'sqoopuser' \
 --password 'NHkkP876rp' \
 --table rrk_list2 \
---export-dir hdfs://cxln1.c.thelab-240901.internal:8020/user/bigdatacloudxlab27228/hdfs_csv \
+--export-dir hdfs://cxln1.c.thelab-240901.internal:8020/user/bigdatacloudxlab27228/hdfs_tsv \
 --input-fields-terminated-by ',' \
 --input-lines-terminated-by '\n' \
 -m1
